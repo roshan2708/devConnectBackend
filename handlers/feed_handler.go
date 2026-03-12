82899db039678321f5150f519741735ed058e874
Roshan Singh <rs602543@gmail.com>
2026-03-12 21:31:07 +0530

    chore: refactored the feed handler

--- handlers/feed_handler.go
+++ handlers/feed_handler.go
@@ -12,12 +12,31 @@ func GetFeed(w http.ResponseWriter, r *http.Request) {
 	session, _ := gothic.Store.Get(r, "devconnect-session")
 	currentUser := session.Values["user_id"].(string)
 	query := `
-	SELECT posts.id, posts.user_id, posts.content, posts.created_at
-	FROM posts
-	JOIN follows
-	ON posts.user_id = follows.following_id
-	WHERE follows.follower_id = $1
-	ORDER BY posts.created_at DESC
+	SELECT 
+posts.id,
+posts.user_id,
+posts.content,
+posts.created_at,
+COUNT(DISTINCT likes.id)*2 + COUNT(DISTINCT comments.id)*3 AS score
+
+FROM posts
+
+JOIN follows
+ON posts.user_id = follows.following_id
+
+LEFT JOIN likes
+ON posts.id = likes.post_id
+
+LEFT JOIN comments
+ON posts.id = comments.post_id
+
+WHERE follows.follower_id = $1
+
+GROUP BY posts.id
+
+ORDER BY score DESC, posts.created_at DESC
+
+LIMIT 20
 	`
 	rows, err := config.DB.Query(query, currentUser)
 	if err != nil {
@@ -30,12 +49,13 @@ func GetFeed(w http.ResponseWriter, r *http.Request) {
 		UserID    string `json:"user_id"`
 		Content   string `json:"content"`
 		CreatedAt string `json:"created_at"`
+		Score     int    `json:"score"`
 	}
 	var posts []Post
 	for rows.Next() {
-		var post Post
-		rows.Scan(&post.ID, &post.UserID, &post.Content, &post.CreatedAt)
-		posts = append(posts, post)
+		var p Post
+		rows.Scan(&p.ID, &p.UserID, &p.Content, &p.CreatedAt, &p.Score)
+		posts = append(posts, p)
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(posts)
